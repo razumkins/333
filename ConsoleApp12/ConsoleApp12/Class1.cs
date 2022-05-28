6a23e716d4d0a7ab4b073b85620c5f7c1afcc598
Haritonova Olga <haritonova.olyakpc2002020@gmail.com>
2022-05-28 12:29:41 +0300

    123

---
+++ ConsoleApp12/ConsoleApp12/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3bcv
+{
+    class Classdrob
+    {
+        int chislitel;
+        int znaminatel;
+        public Classdrob(int chislitel, int znaminatel)
+        {
+            this.chislitel = chislitel;
+            this.znaminatel = znaminatel;
+        }
+        public Classdrob(int chislo)
+        {
+            this.chislitel = chislo;
+            this.znaminatel = 1;
+        }
+        public Classdrob(int celoe, int chislitel, int znamenatel)
+        {
+            this.chislitel = celoe * znamenatel + chislitel;
+            this.znaminatel = znamenatel;
+        }
+        public double ToDouble()
+        {
+            return (Double)(this.chislitel) / this.znaminatel;
+
+
+        }
+        public static Classdrob operator *(Classdrob one, Classdrob two)
+        {
+            return new Classdrob(one.chislitel * two.chislitel, one.znaminatel * two.znaminatel);
+
+        }
+        public bool Znak
+        {
+            get
+            {
+                bool a = chislitel >= 0;
+                bool b = znaminatel >= 0;
+                return a == b;
+
+            }
+            public delegate void pofi(Classdrob ofd, int znah);
+    }
+
+}
+}
---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp3bcv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Classdrob d1 = new Classdrob(2, 3);
+            Classdrob d2 = new Classdrob(-2);
+            Classdrob d3 = new Classdrob(2, 3, 4);
+            Console.WriteLine(d1.ToDouble());
+            Console.WriteLine((d1 * d3).ToDouble());
+            Console.WriteLine((d1 * d2).Znak);
+            Console.ReadKey();
+
+        }
+
+    }
+}}
